a8337f081b1ea6b2fc36af1c0aa282313b885f5b
unknown <>
2016-09-05 16:00:18 +0800

    test

---
+++ libs/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python  
+# -*- coding:utf-8 -*-  
+# File Name: __init__.py
+# Author: meczhang
+# Mail: mecforlove@outlook.com
+# Created Time: 2016-08-23 20:19:16
+
---
+++ libs/utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python  
+# -*- coding:utf-8 -*-  
+# File Name: utils.py
+# Author: meczhang
+# Mail: mecforlove@outlook.com
+# Created Time: 2016-08-13 21:25:19
+
+import urllib2
+
+
+
+def get_opener():
+	"""获取要使用的opener"""
+	# 设置代理
+	proxy = {'http': 'proxy.tencent.com:8080'}
+        proxy_support = urllib2.ProxyHandler(proxy)
+	opener = urllib2.build_opener(proxy_support)
+    # retunr urllib2.build_opener()
+	return opener
+
